7c79f1fe91f0339086cf572f6e7cf1e3a93950b3
Sven <continentcraft4@gmail.com>
2023-02-03 07:55:26 -0700

    finished Whack-a-Mole

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 let score = 0;
+const holes = document.getElementsByClassName("hole")
+
+setInterval(function(){
+    const randomHoleIndex = Math.floor(Math.random()*holes.length)
+    holes[randomHoleIndex].classList.toggle("mole")
+},100)
+
+const gameArea = document.getElementById('whack-a-mole');
+gameArea.addEventListener('click', function(clickEvent) {
+  if (clickEvent.target.matches('.mole')) {
+    clickEvent.target.classList.remove('mole')
+    score++
+    document.getElementById("score").innerText = score
+  }
+});
